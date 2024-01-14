08b0805700b39b87d3f70aa8e6efc2c0e565488f
VMSvaymaks <mazaev.maks.vm@gmail.com>
2024-01-14 16:40:38 +0400

    script add

---
+++ script.js
@@ -0,0 +1,32 @@
+let num = 100;
+if (num < 50) {
+  console.log("<50");
+} else if (num > 20) {
+  console.log("<20");
+} else {
+  console.log("error 404");
+}
+
+let name = "maria's";
+let resultCondition1 = "maria" ? "true" : "false";
+console.log(resultCondition1);
+
+let el5 = 5;
+let resultCondition = el5 > 4 ? "true" : "false";
+console.log(resultCondition);
+
+const name2 = "maria's";
+switch (name2) {
+  case "elena":
+    console.log(false);
+    break;
+  case "marco":
+    console.log(false);
+    break;
+  case "maria's":
+    console.log(true);
+    break;
+  default:
+    console.log(error);
+}
+
